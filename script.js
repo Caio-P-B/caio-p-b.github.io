edfd3c113966b5c4d25feeb3b476f1e1a74b91eb
Caio P. Bearari <72236280+Caio-P-B@users.noreply.github.com>
2025-12-07 00:58:35 +0000

    Update em um dos projetos em destaque

--- script.js
+++ script.js
@@ -240,7 +240,7 @@ const projectDetails = [
   {
     title: "TabNews Clone",
     image:
-      "https://unsplash.com/pt-br/fotografias/pessoa-segurando-iphone-preto-4-0aRycsfH57A",
+      "https://images.unsplash.com/photo-1584438784894-089d6a62b8fa?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     tags: ["React", "Node.js", "Next.js"],
     description: "Implementação do site TabNews.",
     features: [
